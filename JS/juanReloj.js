183c784809814ad31d62f3e2441c35948a456465
Juan Delgado <Delgado-juan@hotmail.com>
2024-10-06 00:03:52 -0500

    adding the date

--- JS/juanReloj.js
+++ JS/juanReloj.js
@@ -1,11 +1,24 @@
 let date = new Date();
 let dateNumber = date.getDate();
-let day = date.toLocaleDateString('es-US', {weekday: 'long'});
-let month = date.toLocaleDateString('es-US', {month: 'long'});
+let day =
+    date
+        .toLocaleDateString('es-US', {weekday: 'long'})
+        .charAt(0)
+        .toUpperCase() +
+    date.toLocaleDateString('es-US', {weekday: 'long'}).slice(1);
+
+let month =
+    date
+        .toLocaleDateString('es-US', {month: 'long'})
+        .charAt(0)
+        .toLocaleUpperCase() +
+    date.toLocaleDateString('es-US', {month: 'long'}).slice(1);
 let year = date.getFullYear();
 console.log(year);
 
 let dateEl = document.createElement('h2');
 dateEl.textContent = `${day}, ${dateNumber} ${month} de ${year}`;
 
-console.log(dateEl);
+let dateSection = document.getElementById('dateSection');
+
+dateSection.appendChild(dateEl);
